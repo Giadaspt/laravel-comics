62f4d5ae07b56e885bf5e805d0ae0e1e94523262
Giadaspt <saputo.giada@gmail.com>
2022-01-21 17:14:00 +0100

    nav active

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,4 @@
+@dump(Route::currentRoutName())
 
 <header class="container mt-4">
   <div class="image">
@@ -8,34 +9,44 @@
   <nav>
     <ul>
       <li>
-        <a href="/characters" {{ route('characters') }}>  characters </a>
+        <a class="{{ (Route::currentRouteName() === 'characters') ? 'active' : '' }}"
+        href="/characters" {{ route('characters') }}>  characters </a>
       </li>
       <li>
-        <a href="/comics" {{ route('comics') }}>  comics </a>
+        <a class="{{ (Route::currentRouteName() === 'comics') ? 'active' : '' }}"
+        href="/comics" {{ route('comics') }}>  comics </a>
       </li>
       <li>
-        <a href="/movie" {{ route('movie') }}>  movie</a>
+        <a class="{{ (Route::currentRouteName() === 'movie') ? 'active' : '' }}"
+        href="/movie" {{ route('movie') }}>  movie</a>
       </li>
       <li>
-        <a href="/tv" {{ route('tv') }}>  tv </a>
+        <a class="{{ (Route::currentRouteName() === 'tv') ? 'active' : '' }}"
+        href="/tv" {{ route('tv') }}>  tv </a>
       </li>
       <li>
-        <a href="/games" {{ route('games') }}>  games </a>
+        <a class="{{ (Route::currentRouteName() === 'games') ? 'active' : '' }}"
+        href="/games" {{ route('games') }}>  games </a>
       </li>
       <li>
-        <a href="/collectibles" {{ route('collectibles') }}>  collectibles </a>
+        <a class="{{ (Route::currentRouteName() === 'collectibles') ? 'active' : '' }}"
+        href="/collectibles" {{ route('collectibles') }}>  collectibles </a>
       </li>
       <li>
-        <a href="/videos" {{ route('videos') }}>  videos </a>
+        <a class="{{ (Route::currentRouteName() === 'videos') ? 'active' : '' }}"
+        href="/videos" {{ route('videos') }}>  videos </a>
       </li>
       <li>
-        <a href="/fans" {{ route('fans') }}>  fans </a>
+        <a class="{{ (Route::currentRouteName() === 'fans') ? 'active' : '' }}"
+        href="/fans" {{ route('fans') }}>  fans </a>
       </li>
       <li>
-        <a href="/news" {{ route('news') }}>  news </a>
+        <a class="{{ (Route::currentRouteName() === 'news') ? 'active' : '' }}"
+        href="/news" {{ route('news') }}>  news </a>
       </li>
       <li>
-        <a href="/shop" {{ route('shop') }}>  shop </a>
+        <a class="{{ (Route::currentRouteName() === 'shop') ? 'active' : '' }}"
+        href="/shop" {{ route('shop') }}>  shop </a>
       </li>
     </ul>
   </nav>
